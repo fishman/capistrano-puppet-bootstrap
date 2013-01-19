46d2c25219b23f4531c8332504df04ef883ce7e1
Reza Jelveh <toyb0mb@gmx.net>
2013-01-18 22:04:46 -0600

    added deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,83 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require "rvm/capistrano"
+require "bundler/capistrano"
+
+set :application, "cajuncodefest"
+set :repository,  "git@myrepo.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, "user"
+set :group, "www-data"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "codefest"                          # Your HTTP server, Apache/etc
+role :app, "codefest"                          # This may be the same as your `Web` server
+role :db,  "codefest", :primary => true # This is where Rails migrations will run
+
+set :deploy_via, :copy
+
+set :rvm_type, :system
+
+default_run_options[:pty] = true
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+after "deploy:setup", :setup_group
+task :setup_group do
+  try_sudo "gem install bundler"
+  try_sudo "chown -R :#{group} #{deploy_to}"
+  try_sudo "chmod -R g+s #{deploy_to}"
+end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  task :start do
+    run "#{try_sudo} service thin start"
+  end
+  task :stop do
+    run "#{try_sudo} service thin stop"
+  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} service thin restart"
+  end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+  task :after_update_code, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/system/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+namespace :bootstrap do
+  task :default do
+    # Set the default_shell to "bash" so that we don't use the RVM shell which isn't installed yet...
+    set :default_shell, "bash"
+    # We tar up the puppet directory from the current directory -- the puppet directory within the source code repository
+    system("tar czf 'puppet.tgz' puppet/")
+    upload("puppet.tgz","/home/#{user}",:via => :scp)
+
+    # Untar the puppet directory, and place at /etc/puppet -- the default location for manifests/modules
+    run("tar xzf puppet.tgz")
+    try_sudo("rm -rf /etc/puppet")
+    try_sudo("mv /home/#{user}/puppet/ /etc/puppet")
+
+    # Bootstrap RVM/Puppet!
+    try_sudo("bash /etc/puppet/bootstrap.sh")
+    try_sudo("useradd -u 52 puppet")
+    try_sudo("adduser rjelvah www-data")
+  end
+end
+
+namespace :puppet do
+  task :default do
+    # Specific RVM string for managing Puppet; may or may not match the RVM string for the application
+    set :rvm_ruby_string, '1.9.3-p125'
+
+    # We tar up the puppet directory from the current directory -- the puppet directory within the source code repository
+    system("tar czf 'puppet.tgz' puppet/")
+    upload("puppet.tgz","/home/#{user}",:via => :scp)
+
+    # Untar the puppet directory, and place at /etc/puppet -- the default location for manifests/modules
+    run("tar xzf puppet.tgz")
+    try_sudo("rm -rf /etc/puppet")
+    try_sudo("mv puppet/ /etc/puppet")
+
+    # Run RVM/Puppet!
+    try_sudo("puppet apply /etc/puppet/manifest.pp")
+  end
+end
